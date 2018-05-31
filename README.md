e49a94d18e4de3b3d948ea6618a8f044b210205b
Ibrahim Nurandita Isbandiputra <ibrahimker@gmail.com>
2018-05-31 18:02:45 +0700

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# UltahDhanty24
+#Dhanty 24th Birthday Website
+This is a website for dhanty 24th birthday. you can access the live in here
+[https://ibrahimker.id/lab/dhanty24/](https://ibrahimker.id/lab/dhanty24/)
---
+++ index.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+  <title>Happy Birthday Dhanty 24</title>
+  <link rel="stylesheet" href="style.css"/>
+  <link rel="stylesheet" href="cake.css"/>
+</head>
+<body>
+  <div class='birthday'>
+    <div class='birthday_inner'>
+      <div class='birthday_inner__modal'>
+        <div class='modal_image'>
+          <img src='/'>
+        </div>
+        <div class='modal_content'>
+          <h1>Title</h1>
+          <p>Content</p>
+          <button>Kuy</button>
+        </div>
+      </div>
+      <div class='birthday_inner__stage stage1'>
+        <div class='intro_cake'>
+          <!-- <img class='intro' src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/cake.png'> -->
+          <div class="cake2">
+            <div class="plate2"></div>
+            <div class="layer2 layer-bottom2"></div>
+            <div class="layer2 layer-middle2"></div>
+            <div class="layer2 layer-top2"></div>
+            <div class="icing2"></div>
+            <div class="drip2 drip12"></div>
+            <div class="drip2 drip22"></div>
+            <div class="drip2 drip32"></div>
+            <div class="candle2">
+              <div class="flame2"></div>
+            </div>
+          </div>
+        </div>
+        <button class='start'>Ayo Buat Kue</button>
+      </div>
+      <div class='birthday_inner__stage stage2'>
+        <h1>Aduk Adonan</h1>
+        <h2>Untuk mulai bikin, ayo kita aduk adonan kuenya hingga sempurna. Klik tombol dibawah sampai adonan teraduk dengan baik.</h2>
+        <div class='mix'>
+          <div class='mix_bowl'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/bowl.png'>
+          </div>
+          <div class='mix_spoon'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/spoon.png'>
+          </div>
+        </div>
+        <button class='mixer'>Aduk!!!</button>
+      </div>
+      <div class='birthday_inner__stage stage3'>
+        <h1>Panggang Kuenya</h1>
+        <h2>Yeah setelah selesai diaduk, saatnya kita bakar kuenya di oven. Drag adonan ke oven. Lalu tunggu hingga matang.</h2>
+        <div class='oven'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/oven.png'>
+        </div>
+        <div class='tin'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tin.png'>
+        </div>
+      </div>
+      <div class='birthday_inner__stage stage4'>
+        <h1>Mari Berkreasi</h1>
+        <h2>Sudah matang, saatnya menghias dan memberikan warna. Klik rasa kue yang kamu mau, jangan lupa tambahin icing nya. Pasang lilin nya jika sudah selesai kue nya!</h2>
+        <div class='svg'>
+          Happy Birthday Dhanty
+        </svg>
+      </div>
+      <div class='options'>
+        <div class='sponges inactive'>
+          <h3>Sponge</h3>
+          <h5>
+            <span>0</span>
+            / 5
+          </h5>
+          <div class='item'>
+            <div class='item_inner vanilla'></div>
+            <h4>Vanilla</h4>
+          </div>
+          <div class='item'>
+            <div class='item_inner strawberry'></div>
+            <h4>Strawberry</h4>
+          </div>
+          <div class='item'>
+            <div class='item_inner fruit'></div>
+            <h4>Tutti Frutty</h4>
+          </div>
+        </div>
+        <div class='fillings'>
+          <h3>Icing</h3>
+          <h5>
+            <span>0</span>
+            / 6
+          </h5>
+          <div class='item'>
+            <div class='item_inner jam'></div>
+            <h4>Selai</h4>
+          </div>
+          <div class='item'>
+            <div class='item_inner cream'></div>
+            <h4>Krim</h4>
+          </div>
+          <div class='item'>
+            <div class='item_inner choc'></div>
+            <h4>Coklat</h4>
+          </div>
+          <div class='item'>
+            <div class='item_inner lemon'></div>
+            <h4>Jeruk</h4>
+          </div>
+        </div>
+      </div>
+      <div class='cakemake'>
+        <div class='base'></div>
+      </div>
+      <div class='startagain'>Mulai Lagi</div>
+      <button class='add'>Taruh Lilin</button>
+      <button class='sa'>Mulai Lagi</button>
+    </div>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
